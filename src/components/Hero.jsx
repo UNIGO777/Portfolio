67c41957bc06c6fa88349bc037b3e2333883aab9
UNIGO777 <naman13399@gmail.com>
2024-03-29 00:52:49 +0530

    complete final

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,12 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
-      <ComputersCanvas />
+      <div className="hidden md:inline ">
+        <ComputersCanvas />
+      </div>
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[30px] h-[60px] rounded-3xl border-4 border-[#ededed] flex justify-center items-start p-2">
+          <div className="w-[30px] h-[60px] rounded-3xl border-4 border-[#ededed] text-white flex justify-center items-start p-2">
             <motion.dev
               animate={{
                 y: [0, 24, 0],
@@ -29,7 +30,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType:"loop"
               }}
-              className="w-3 h-2 rounded-full  mb-1 bg-transparent"
+              className="w-3 h-2 rounded-full bg-white  mb-1 bg-transparent"
             />
 
           </div>
